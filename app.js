aeb64e31d96fa5e2f20cb53e43c1c0d883dc1cf2
SyedBashar <MhuammadBashar@gmail.com>
2024-10-15 19:18:43 +0500

    Task Marks Done

---
+++ app.js
@@ -0,0 +1,18 @@
+
+    
+    var marks = parseInt(prompt("Please enter your marks:"));
+
+    if (marks >= 90) {
+        console.log("Grade A");
+    } else if (marks >= 80) {
+        console.log("Grade B");
+    } else if (marks >= 70) {
+        console.log("Grade C");
+    } else if (marks >= 60) {
+        console.log("Grade D");
+    } else {
+        console.log("Grade F");
+    }
+
+
+assignGrade();
